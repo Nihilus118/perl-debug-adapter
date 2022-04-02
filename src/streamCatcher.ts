5137c97d1c10d0e6a24974f244748cb2285233f6
Nihilus118 <johannes.ritter.ma@gmail.com>
2022-04-02 21:00:14 +0200

    fix weird line split and trim lines

--- src/streamCatcher.ts
+++ src/streamCatcher.ts
@@ -1,7 +1,25 @@
 import { Writable, Readable } from 'stream';
 import { EventEmitter } from 'events';
-import { platform } from 'os';
-import * as RX from './regExp';
+
+const colors = /\u001b\[([0-9]+)m|\u001b/g;
+
+const db = /^(\[(pid=)?[0-9\->]+\])?(\[\d+\])?DB\<+([0-9]+)\>+$/;
+
+function cleanLine(line: string) {
+    return line.replace(colors, '').replace(/\s|(\\b)/g, '').replace('\b', '');
+}
+
+const lastCommandLine = {
+    test(line: string) {
+        const stripped = cleanLine(line);
+        return db.test(stripped);
+    },
+
+    match(line: string) {
+        const stripped = cleanLine(line);
+        return stripped.match(db);
+    }
+};
 
 interface RequestTask {
     command: string | null,
@@ -18,12 +36,6 @@ export class StreamCatcher extends EventEmitter {
 
     public input!: Writable;
 
-    logDebug(...args: any[]) {
-        if (this.debug) {
-            console.log(...args);
-        }
-    }
-
     constructor() {
         super();
     }
@@ -32,73 +44,29 @@ export class StreamCatcher extends EventEmitter {
         this.input = input;
 
         let lastBuffer = '';
-        let timeout: NodeJS.Timer | null = null;
         output.on('data', (buffer) => {
-
-            this.logDebug('RAW:', buffer.toString());
-            this.emit('perl-debug.streamcatcher.data', buffer.toString());
-
             const data = lastBuffer + buffer.toString();
-            const lines = data.split(/\r\n|\r|\n/);
-            const firstLine = lines[0];
+            const lines = data.split(/\r?\n/);
             const lastLine = lines[lines.length - 1];
-            const commandIsDone = RX.lastCommandLine.test(lastLine);
 
-            // xxx: Windows restart workaround
-            // the windows perl debugger doesn't end the current restart request so we have to
-            // simulate a proper request end.
-            if ((platform() === "win32" && RX.restartWarning.test(firstLine)) || timeout) {
-
-                if (RX.restartWarning.test(firstLine)) {
-                    this.logDebug('RAW> Waiting to fake end of restart request');
-                }
-
-                if (timeout) {
-                    clearTimeout(timeout);
-                }
-                timeout = setTimeout(() => {
-                    timeout = null;
-                    if (this.requestRunning) {
-                        this.logDebug('RAW> Fake end of restart request');
-                        // xxx: We might want to simulate all the restart output
-                        this.readline('   DB<0> ');
-                    }
-                }, 500);
-            }
-
-            if (/\r\n|\r|\n$/.test(data) || commandIsDone) {
+            if (lastCommandLine.test(lastLine)) {
                 lastBuffer = '';
             } else {
                 lastBuffer = lines.pop()!;
             }
             lines.forEach(line => this.readline(line));
         });
-        output.on('close', () => {
-            // xxx: Windows perl debugger just exits on syntax error without "DB<n>"
-            // If theres stuff left in the buffer we push it and end the request.
-            if (this.requestRunning) {
-                this.logDebug('RAW> Fake end of request');
-                this.readline(lastBuffer);
-                this.readline('Debugged program terminated.  Use q to quit or R to restart,');
-                this.readline('use o inhibit_exit to avoid stopping after program termination,');
-                this.readline('h q, h R or h o to get additional info.');
-                this.readline('   DB<0> ');
-            }
-        });
 
         return this.request(null);
     }
 
     readline(line: string) {
-        this.logDebug('line:', line);
-        // this.logDebug('data:', [...line]);
+        // Remove unnecessary tabs and spaces
+        line = line.trim();
         this.buffer.push(line);
-        // Test for command end
-        if (RX.lastCommandLine.test(line)) {
-            this.logDebug('END:', line);
-            const data = this.buffer;
+        if (lastCommandLine.test(line)) {
+            const data = this.buffer.filter(e => { return e !== ''; });
             this.buffer = [];
-            // xxx: We might want to verify the DB nr and the cmd number
             this.resolveRequest(data);
         }
     }
@@ -120,21 +88,15 @@ export class StreamCatcher extends EventEmitter {
 
     nextRequest() {
         if (!this.requestRunning && this.requestQueue.length) {
-            // Set new request
             this.requestRunning = this.requestQueue.shift()!;
-            // this.logOutput(`NEXT: ${this.requestRunning.command}\n`);
-            // a null command is used for the initial run, in that case we don't need to
-            // do anything but listen
             if (this.requestRunning.command !== null) {
                 const data = `${this.requestRunning.command}\n`;
-                this.emit('perl-debug.streamcatcher.write', data);
                 this.input.write(data);
             }
         }
     }
 
     request(command: string | null): Promise<string[]> {
-        this.logDebug(command ? `CMD: "${command}"` : 'REQ-INIT');
         return new Promise((resolve, reject) => {
             // Add our request to the queue
             this.requestQueue.push({
